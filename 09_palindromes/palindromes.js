969772f78d9854dbea44f35ace6ba006e69edbb7
Erick Silva <106249621+Ericksilvadeoliveira@users.noreply.github.com>
2022-10-03 04:37:43 -0300

    palindromes finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    var regularEx = /[^A-Za-z0-9]/g;
 
-};
+    var lowRegStr = str.toLowerCase().replace(regularEx, '');
 
+    var reverseStr = lowRegStr.split('').reverse().join('');
+
+    return reverseStr === lowRegStr;
+}
 // Do not edit below this line
 module.exports = palindromes;
