f292913c3706a7ad7667ed381aac539e76b6b8ef
Erick Silva <106249621+Ericksilvadeoliveira@users.noreply.github.com>
2022-10-03 05:29:51 -0300

    start fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
 const fibonacci = function() {
+    var sequence = [];
 
+    sequence[0] = 0;
+    sequence[1] = 1;
+    
+    for (i = 2; i < 10; i++) {
+        sequence[i] = sequence[i - 2] + sequence[i - 1];
+    }
+    return sequence[6];
 };
 
 // Do not edit below this line
